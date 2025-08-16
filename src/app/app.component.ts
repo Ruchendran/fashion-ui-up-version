8d4774f4a8aceed3c9f02d690df09042fd050b01
Chathruknan Sathvathu <vvruchendran141594@gmail.com>
2025-08-16 09:00:29 +0530

    first changes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit,ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HostListener} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,26 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit  {
   title = 'fashion-ui';
+  constructor(){
+
+  }
+  @ViewChild('signInHover') signInHover!:ElementRef;
+  @HostListener('click',['$event'])
+  onClick=(outOfHover:Event)=>{
+    if(!this.signInHover.nativeElement.contains(outOfHover.target)){
+      let signInHoverElement=document.getElementById('sign-in-hover');
+      signInHoverElement?.classList.remove('sign-in-hover-label');
+      signInHoverElement?.classList.add('sign-in-hover');
+    }
+  }
+  ngOnInit(): void {
+  
+  }
+  pointHover=()=>{
+    let signInHoverElement=document.getElementById('sign-in-hover');
+    signInHoverElement?.classList.add('sign-in-hover-label');
+  }
+
 }
