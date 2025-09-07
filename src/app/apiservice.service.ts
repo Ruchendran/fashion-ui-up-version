98274e75aa920a884ee6418ecf7b439e87ec93a8
Chathruknan Sathvathu <vvruchendran141594@gmail.com>
2025-09-07 11:28:56 +0530

    loader has included 07-09-2025.

--- src/app/apiservice.service.ts
+++ src/app/apiservice.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient} from '@angular/common/http';
 import { serverVal } from '../../environment';
 import { Observable } from 'rxjs';
 
@@ -10,6 +10,10 @@ export class ApiserviceService implements OnInit {
 
   constructor(private http:HttpClient) { }
 
+  apiConstants={
+    registerUser:serverVal.server+'/auth/register',
+    loginUser:serverVal.server+'/auth/login'
+  }
 
   ngOnInit(): void {
       
@@ -19,5 +23,11 @@ export class ApiserviceService implements OnInit {
     return this.http.post(serverVal.server+"/admin/upload", data);  // POST request
   }
 
-  
+  registerUser(data:any){
+    return this.http.post(this.apiConstants.registerUser,data);
+  }
+  loginUser(data:any){
+    console.log("hit");
+    return this.http.post(this.apiConstants.loginUser,data);
+  }
 }
--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -1,16 +1,87 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ComponentRef, OnInit, ViewChild,ViewContainerRef } from '@angular/core';
+import { ReactiveFormsModule,FormGroup,FormControl, Validators} from '@angular/forms';
+import { ApiserviceService } from '../apiservice.service';
+import { LoaderComponent } from '../loader/loader.component';
 
 @Component({
   selector: 'app-log-in',
-  imports: [CommonModule],
+  imports: [CommonModule,ReactiveFormsModule,LoaderComponent],
   templateUrl: './log-in.component.html',
   styleUrl: './log-in.component.scss'
 })
-export class LogInComponent {
+export class LogInComponent implements OnInit {
+  constructor(private apiService:ApiserviceService,private containerRef:ViewContainerRef){}
+  @ViewChild('loaderContainer') loaderContainer!:ComponentRef<any>;
+  userForm:any;
+  register=false;
   eyeOpen=false;
+  // cardList=[
+  //   {
+  //     name:"Gents Fashion",
+  //     label:"Gents Fashion"
+  //    },
+  //    {
+  //     name:"Shirts",
+  //     label:'Shirts'
+  //    },
+  //    {
+  //     name:"Pants",
+  //     label:"Pants"
+  //    },
+  //    {
+  //     name:"Jeans",
+  //     label:"Jeans"
+  //    },
+  //    {
+  //     name:"Smooth",
+  //     label:"Smooth"
+  //    }
+  // ]
   onClickeye(){
     console.log("hit")
     this.eyeOpen=!this.eyeOpen;
+  }
+  ngOnInit(): void {
+    this.userForm=new FormGroup({
+      username:new FormControl('',Validators.required),
+      password:new FormControl('',Validators.required),
+      phone:new FormControl('',Validators.required)
+    })
+  }
+  onRegister(){
+    this.register=!this.register
+  }
+  onSubmit=()=>{
+    this.showLoader();
+    //This is only for Register users.
+    if(this.register){
+      this.apiService.registerUser(this.userForm.value).subscribe((res:any)=>{
+        this.hideLoader();
+        alert(res.message)
+      },
+      (error)=>{
+        this.hideLoader();
+        alert("Something went wrong!!!!!!!");
+      })
+    }
+    //This is only for login users.
+    else{
+      this.apiService.loginUser(this.userForm.value).subscribe((res:any)=>{
+        this.hideLoader();
+        alert(res.message)
+      },
+      (error)=>{
+        this.hideLoader();
+        alert("Something went wrong!!!!!!!");
+      }
+    )
+    }
+  }
+  showLoader=()=>{
+   this.loaderContainer=this.containerRef.createComponent(LoaderComponent) 
+  }
+  hideLoader=()=>{
+    this.loaderContainer.destroy();
   }
 }
